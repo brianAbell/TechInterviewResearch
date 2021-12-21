8ce439a8df5faee3ce591e8d38253a595e60d00e
bbell.semm@gmail.com <brianbell212@gmail.com>
2021-12-21 13:17:36 -0500

    Array & DP work added

---
+++ src/com/company/Sets.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class Sets {
+    public static void main(String[] args) {
+        String[] things = {"apple", "bob", "ham", "bob", "bacon"};
+        List<String> list = Arrays.asList(things);
+
+        System.out.printf("%s ", list);
+        System.out.println();
+
+        //using hashset to remove any duplicates within the given set
+        Set<String> set = new HashSet<String>(list);
+        System.out.printf("%s ", set);
+    }
+}
