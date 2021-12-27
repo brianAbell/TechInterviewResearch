72972477dd9f0180b6a3477d6c7df5040df85eff
Brian <brianbell212@gmail.com>
2021-12-27 13:39:22 -0500

    Create improved linked list class example

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,47 +1,150 @@
 package com.company;
 
-import org.w3c.dom.Node;
+class LinkedList
+{
+    //Class variables for the Linked List
+    private static Node head;
+    private static int numNodes;
 
-public class LinkedList {
-    Node head;
-
-    public void append(int data) {
-        if (head == null) {
-            head = new Node(data);
-            return;
-        }
-
-        Node current = head;
-        while(current.next != null) {
-            current = current.next;
-        }
-        current.next = new Node(data);
+    public LinkedList(Object dat)
+    {
+        head = new Node(dat);
     }
 
-    public void prepend(int data) {
-        Node newHead = new Node(data);
-        newHead.next = head;
-        head = newHead;
+    public void addAtHead(Object dat)
+    {
+        Node temp = head;
+        head = new Node(dat);
+        head.next = temp;
+        numNodes++;
     }
 
-    public void deleteWithValue(int data) {
-        if(head == null) {return;}
-        if(head.data == data) {
-            head = head.next;
-            return;
+    public void addAtTail(Object dat)
+    {
+        Node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
         }
 
-        Node current = head;
+        temp.next = new Node(dat);
+        numNodes++;
+    }
 
-        while(current.next != null) {
-            if(current.next.data == data) {
-                current.next = current.next.next;
-                return;
-            }
-            current = current.next;
+    public void addAtIndex(int index, Object dat)
+    {
+        Node temp = head;
+        Node holder;
+        for(int i=0; i < index-1 && temp.next != null; i++)
+        {
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(dat);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public void addAtIndex(int index, Node node)
+    {
+        Node temp = head;
+        Node holder;
+        for(int i=0; i < index-1 && temp.next != null; i++)
+        {
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = node;
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public void deleteAtIndex(int index)
+    {
+        Node temp = head;
+        for(int i=0; i< index - 1 && temp.next != null; i++)
+        {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        numNodes--;
+    }
+
+    public void switchWithNext(int index)
+    {
+        if(index != 0)
+        {
+            Node bOne = find(index - 1);
+            Node one = find(index);
+            Node two = one.next;
+
+            one.next = two.next;
+            bOne.next = two;
+            two.next = one;
+        }
+        else
+        {
+            Node one = find(index);
+            Node two = one.next;
+
+            head = two;
+            one.next = two.next;
+            two.next = one;
         }
     }
 
+    public static int find(Node n)
+    {
+        Node t = head;
+        int index = 0;
+        while(t != n)
+        {
+            index++;
+            t = t.next;
+        }
+        return index;
+    }
 
+    public static Node find(int index)
+    {
+        Node temp=head;
+        for(int i=0; i<index; i++)
+        {
+            temp = temp.next;
+        }
+        return temp;
+    }
 
+    public static void printList()
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + ",");
+            temp = temp.next;
+        }
+        System.out.print("\n");
+    }
+
+    public static int getSize()
+    {
+        return numNodes;
+    }
+
+    class Node
+    {
+        //Declare class variables
+        private Node next;
+        private Object data;
+
+        public Node(Object dat)
+        {
+            data = dat;
+        }
+
+        public Object getData()
+        {
+            return data;
+        }
+    }
 }
