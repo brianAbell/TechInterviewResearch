457f6dbb66399bae1cd70a04b33cc5ae1ef2382d
Brian <brianbell212@gmail.com>
2022-03-30 14:11:07 -0400

    Added java file for testing solutions

---
+++ Notes/Leetcode/blind75.java
@@ -0,0 +1,18 @@
+public class blind75 {
+
+    public static void main(String[] args) {
+        twosum();
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
